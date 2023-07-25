5bc2e7552de0863bc6ce6c4e7fcedf20fd9a6b56
Cristhian Cedeño <daniel49571@gmail.com>
2023-07-25 13:51:40 +0000

    jojo

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 #include <cryptopp/des.h>
 #include <cryptopp/base64.h>
 #include <mpi.h>
+#include <fstream>
+
+
+std::string leerArchivo(){
+    std::ifstream file("archivo.txt");
+    std::string content;
+
+    if (file.is_open()) {
+        file.seekg(0, std::ios::end);
+        content.reserve(file.tellg());
+        file.seekg(0, std::ios::beg);
+        content.assign((std::istreambuf_iterator<char>(file)),
+                       std::istreambuf_iterator<char>());
+        fmt::println("rico");
+        file.close();
+    }
+    return content;
+}
+
 
 void DES_decrypt(const char *keyString, CryptoPP::byte *block, size_t length, CryptoPP::byte *out_buffer) {
     CryptoPP::byte key[CryptoPP::DES::KEYLENGTH];
@@ -41,8 +60,10 @@ std::string from_base64(std::string str) {
 
 int main(int argc, char** argv) {
     // texto en formato BASE64
-    std::string cipher_text = "3CC4M6vAfC92tf7vIkRzXC9bOZKtT6q6jz0ewyv4niBO7M8jHcdmi+h2yNq4KRJO2TzgL0dZbcwnrZMHClLJHQ==";
+    std::string cipher_text = "to";
     const char* text_to_find = "cryptographic";
+    //fmt::println("Txt: {}", tex);
+
 
     MPI_Init(&argc, &argv);
 
@@ -52,8 +73,31 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank_id);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    long block_size = LLONG_MAX/nprocs;
+    long block_size = 0;
 
+    if(rank_id == 0){
+        cipher_text = leerArchivo();
+        block_size = LLONG_MAX/nprocs;
+        //MPI_Bcast(&cipher_text, 1, MPI_CHAR,0,MPI_COMM_WORLD);
+
+        int length = cipher_text.length() + 1; // Include null character
+        MPI_Bcast(&length, 1, MPI_INT, 0, MPI_COMM_WORLD); // Broadcast length
+        MPI_Bcast(&cipher_text[0], length, MPI_CHAR, 0, MPI_COMM_WORLD); // Broadcast text
+        MPI_Bcast(&block_size, 1, MPI_LONG, 0 , MPI_COMM_WORLD);
+    } else {
+        int length;
+        MPI_Bcast(&length, 1, MPI_INT, 0, MPI_COMM_WORLD); // Receive length
+        char* buffer = new char[length];
+        MPI_Bcast(buffer, length, MPI_CHAR, 0, MPI_COMM_WORLD); // Receive text
+        std::string receivedText(buffer);
+        MPI_Bcast(&block_size, 1, MPI_LONG, 0 , MPI_COMM_WORLD);
+        cipher_text = buffer;
+        delete[] buffer;
+        //std::cout << "Process " << rank_id << " received: " << receivedText << std::endl;
+        //fmt::println("{}", block_size);
+    }
+    std::cout << "Process " << rank_id << " received: " << cipher_text << std::endl;
+    fmt::println("{}", block_size);
     long start_index = rank_id*block_size;
     long end_index = (rank_id+1)*block_size;
 
@@ -71,6 +115,10 @@ int main(int argc, char** argv) {
     MPI_Status st;
     MPI_Irecv(&key_found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
 
+
+
+
+
     std::string text = from_base64(cipher_text);
     int clen = text.size();
     CryptoPP::byte* ptr = (CryptoPP::byte* )text.c_str();
@@ -81,7 +129,6 @@ int main(int argc, char** argv) {
     int flag;
     for(test_key=start_index;test_key<end_index && !key_found;test_key++) {
         DES_decrypt((char *)&test_key, ptr, clen,out_buffer.data() );
-
         if( std::strstr((char *)out_buffer.data(), text_to_find) ) {
             key_found = test_key;
 
@@ -108,6 +155,8 @@ int main(int argc, char** argv) {
 
     if(rank_id==0) {
         MPI_Wait(&req,&st);
+
+
 
         DES_decrypt((char *)&key_found, ptr, clen,out_buffer.data() );
 
